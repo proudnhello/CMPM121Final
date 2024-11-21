622f9b84b08fa5d7a5f83ab21795229a4e467517
Cheristic <ethanheffan@gmail.com>
2024-11-20 21:52:33 -0800

    Stores grid information in separate class

--- Scripts/Grid/GridManager.cs
+++ Scripts/Grid/GridManager.cs
@@ -2,27 +2,57 @@ using Godot;
 using System;
 
 public struct Cell {
-	int waterLevel;
-	int sunLevel;
-	int plantType;
-	int plantLevel;
+	public int waterLevel;
+	public int sunLevel;
+	public int plantType;
+	public int plantLevel;
+}
+
+public class Grid {
+	public Cell[] currentCells;
+	Cell[] swapCells;
+	int dimensions;
+	
+	public Grid(int _dimensions) {
+		dimensions = _dimensions;
+		currentCells = new Cell[dimensions * dimensions];
+		swapCells = new Cell[dimensions * dimensions];
+	}
+
+	public Cell[] GetCells() {
+		return currentCells;
+	}
+
+	public void Iterate() {
+		
+		// iterate through currentCells and store changes in swapCells, then swap pointers;
+		for (int i = 0; i < currentCells.Length; i++) {
+			int x = i % dimensions;
+			int y = Mathf.FloorToInt(i / dimensions);
+
+			swapCells[i].waterLevel = currentCells[i].waterLevel + 1;
+		}
+
+        (swapCells, currentCells) = (currentCells, swapCells);
+    }
 }
 
 public partial class GridManager : Node
 {
 	[Export(PropertyHint.Range, "1, 200")] public int gridDimensions;
 
-	public Cell[] cells;
 	public GridRenderer GridRenderer;
+	public Grid grid;
 
 	public override void _Ready()
 	{
-		cells = new Cell[gridDimensions * gridDimensions];
+		grid = new Grid(gridDimensions);
 		GridRenderer = GetNode<GridRenderer>("/root/Node2D/GridManager/GridRenderer");
 	}
 
 	public void ProgressTime() {
-		GridRenderer.RenderGrid(cells);
+		grid.Iterate();
+		GridRenderer.RenderGrid(grid.GetCells());
 	}
 }
 
