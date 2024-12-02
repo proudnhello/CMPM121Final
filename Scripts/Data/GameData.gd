3b0b514455cfe869c48c7faa9d1e434a9f048576
Igor Bessa <ibessa@ucsc.edu>
2024-12-01 22:44:10 -0800

    Added JSON loading for importing game information

---
+++ Scripts/Data/GameData.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var itemData = {};
+
+var dataFilepath = "Events/gridevents.json"
+
+func LoadJSONFile(filepath: String):
+	if FileAccess.file_exists(filepath):
+		var dataFile = FileAccess.open(filepath, FileAccess.READ);
+		var parsedResult = JSON.parse_string(dataFile.get_as_text());
+		if parsedResult is Dictionary:
+			return parsedResult;
+		else:
+			print("Error parsing JSON file: ", filepath);
+	else:
+		print("File not found: ", filepath);
+		
+
+func _ready() -> void:
+	itemData = LoadJSONFile(dataFilepath);
+
